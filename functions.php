e9d0fbbd509561cb7147d822af2470279d2353ac
yoza kazuki <yzkzk0323@gmail.com>
2022-05-09 18:54:40 +0900

    remodalの導入

--- functions.php
+++ functions.php
@@ -17,7 +17,9 @@ add_action('wp_enqueue_scripts', function() {
 	
 	$timestamp = date( 'Ymdgis', filemtime( get_stylesheet_directory() . '/style.css' ) );
 	wp_enqueue_style( 'child_style', get_stylesheet_directory_uri() .'/style.css', [], $timestamp );
-
 	/* その他の読み込みファイルはこの下に記述 */
+	wp_enqueue_style( 'remodal_default_style', get_stylesheet_directory_uri() .'/assets/css/remodal-default-theme.css', [], $timestamp );
+	wp_enqueue_style( 'remodal_style', get_stylesheet_directory_uri() .'/assets/css/remodal.css', [], $timestamp );
+	wp_enqueue_script( 'remodal_js', get_stylesheet_directory_uri() .'/assets/js/plugins/remodal.min.js', array('jquery'), '1.0.0', true );
 
 }, 11);
