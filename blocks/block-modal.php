2aa591f1c4a3fd7cf62394dff29d2b4254648416
yoza kazuki <yzkzk0323@gmail.com>
2022-05-09 19:42:48 +0900

    モーダルのレイアウトの修正

--- blocks/block-modal.php
+++ blocks/block-modal.php
@@ -17,15 +17,15 @@ $modal['link'] = block_value('link');
 <div class="remodal" data-remodal-id="modal_<?php echo $modal['modal_id'] ?>">
     <button data-remodal-action="close" class="remodal-close"></button>
     <div class="wp-block-columns wp-block-columns--modal">
-        <div class="wp-block-column">
+
+        <div class="wp-block-column wp-block-column--left">
             <figure class="wp-block-image is-style-shadow">
                 <img src="<?php echo block_field('image') ?>" data-src="" alt="" class="lazyloaded" data-luminous="off">
             </figure>
         </div>
-
-        <div class="wp-block-column">
-            <h2><?php echo $modal['title']; ?></h2>
-            <p><?php echo $modal['desc']; ?></p>
+        <div class="wp-block-column wp-block-column--right">
+            <h2 class="title"><?php echo $modal['title']; ?></h2>
+            <p class="desc"><?php echo $modal['desc']; ?></p>
             <div class="swell-block-button red_ is-style-btn_solid">
                 <a href="<?php echo $modal['link']; ?>" class="swell-block-button__link"><span>詳細を見る</span></a>
             </div>
