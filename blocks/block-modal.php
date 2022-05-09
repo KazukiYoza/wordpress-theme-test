73ae1b9d30a9c2f5fefd4db7d7107609205e1923
yoza kazuki <yzkzk0323@gmail.com>
2022-05-09 19:04:30 +0900

    flexにする

--- blocks/block-modal.php
+++ blocks/block-modal.php
@@ -8,16 +8,24 @@ $modal['link'] = block_value('link');
 
 <figure class="wp-block-image is-style-shadow">
     <a href="#modal_a">
-        <img src="<?php echo block_field('image') ?>" data-src="" alt="" class="lazyloaded"data-luminous="off">
+        <img src="<?php echo block_field('image') ?>" data-src="" alt="" class="lazyloaded" data-luminous="off">
     </a>
 </figure>
 <p><?php echo $modal['title'] ?></p>
 
 <div class="remodal" data-remodal-id="modal_a">
-  <button data-remodal-action="close" class="remodal-close"></button>
-  <h2><?php echo $modal['title']; ?></h2>
-  <p><?php echo $modal['desc']; ?></p>
-  <br>
-  <button data-remodal-action="cancel" class="remodal-cancel">Cancel</button>
-  <button data-remodal-action="confirm" class="remodal-confirm">OK</button>
+    <button data-remodal-action="close" class="remodal-close"></button>
+    <div class="wp-block-columns">
+        <div class="wp-block-column">
+            <figure class="wp-block-image is-style-shadow">
+                <img src="<?php echo block_field('image') ?>" data-src="" alt="" class="lazyloaded" data-luminous="off">
+            </figure>
+        </div>
+
+        <div class="wp-block-column">
+            <h2><?php echo $modal['title']; ?></h2>
+            <p><?php echo $modal['desc']; ?></p>
+            <a href="<?php echo $modal['link']; ?>" class="button is-primary">詳細を見る</a>
+        </div>
+    </div>
 </div>
