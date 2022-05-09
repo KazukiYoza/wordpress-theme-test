4256cea93af7526eed67ed0d7e7a583955af76a1
yoza kazuki <yzkzk0323@gmail.com>
2022-05-09 18:25:24 +0900

    foreach文を削除

--- blocks/block-modal.php
+++ blocks/block-modal.php
@@ -6,14 +6,9 @@ $modal['image'] = block_value('image');
 $modal['link'] = block_value('link');
 ?>
 
-
-<?php foreach ($modal as $key => $value) {
-    if ($value != null) { ?>
-    <figure class="wp-block-image is-style-shadow">
-        <a href="<?php echo $modal['link']; ?>">
-            <img src="<?php echo $modal['image']; ?>" data-src="" alt="" class="lazyloaded"data-luminous="off">
-        </a>
-    </figure>
-    <p><?php echo $modal['title'] ?></p>
-<?php } ?>
-<?php } ?>
+<figure class="wp-block-image is-style-shadow">
+    <a href="<?php echo $modal['link']; ?>">
+        <img src="<?php echo $modal['image']; ?>" data-src="" alt="" class="lazyloaded"data-luminous="off">
+    </a>
+</figure>
+<p><?php echo $modal['title'] ?></p>
